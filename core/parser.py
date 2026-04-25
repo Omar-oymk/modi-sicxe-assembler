47550e85a7e5866e23fad9ef9ad51fbce77560a3
omaroymk <omaroymk@gmail.com>
2026-04-26 00:54:54 +0300

    added a format 3 -> 4 if + is leading the instruction also added the is_valid_instruction() and detected valid instructions while printing the error to the error.txt file

--- core/parser.py
+++ core/parser.py
@@ -4,7 +4,7 @@ import re
 
 intermediate_list = []
 
-def parse_lines(file_path = Path("input") / "in.txt"):
+def parse_lines(file_path = Path(__file__).parents[1] / "input" / "in.txt"):
     with open(file_path, "r") as f:
         lines = f.readlines()
     return lines
@@ -49,15 +49,20 @@ def preprocess_and_print_lines():
         # construct the objects and append them to the intermediate list
         if len(line) == 3:
 
+            
             if not valid_instruction(line[1]):
                 # break
                 # raise an error
                 # and generate an error.txt file with the error message
-                with open(Path("output") / "error.txt", "w") as f:
+                with open(Path(__file__).parents[1]/ 'output' / "error.txt", "w") as f:
                     f.write(f"ERROR OCCURED IN LINE: {i+1}: {line}\nPROGRAM TERMINATED AT PASS 1.")
                 raise ValueError(f"Invalid instruction: {line[1]}")
 
-            opcode = tables.OPCODES.get(line[1].strip().upper())
+            opcode = tables.OPCODES.get(line[1].strip("+").upper())
+
+            if opcode and is_format_4(line[1]):
+                opcode.format = 4
+
             intermediate_list.append(tables.Line(label=line[0],
                                                     Instruction=line[1],
                                                     opcode=opcode if opcode else None,  # none if it is actually a directive
@@ -71,7 +76,7 @@ def preprocess_and_print_lines():
                 # break
                 # raise an error
                 # and generate an error.txt file with the error message
-                with open(Path("output") / "error.txt", "w") as f:
+                with open(Path(__file__).parents[1]/ 'output' / "error.txt", "w") as f:
                     f.write(f"ERROR OCCURED IN LINE: {i+1}: {line}\nPROGRAM TERMINATED AT PASS 1.")
                 raise ValueError(f"Invalid instruction: {line[0]}")
 
@@ -89,7 +94,7 @@ def preprocess_and_print_lines():
                 # break
                 # raise an error
                 # and generate an error.txt file with the error message
-                with open(Path("output") / "error.txt", "w") as f:
+                with open(Path(__file__).parents[1]/ 'output' / "error.txt", "w") as f:
                     f.write(f"ERROR OCCURED IN LINE: {i+1}: {line}\nPROGRAM TERMINATED AT PASS 1.")
                 raise ValueError(f"Invalid instruction: {line[0]}")
 
@@ -108,7 +113,9 @@ def preprocess_and_print_lines():
         print(line)
 
 def valid_instruction(instruction: str):
-    return instruction.upper() in tables.OPCODES or instruction.upper() in tables.DIRECTIVES
+    return instruction.upper() in tables.OPCODES or instruction.upper() in tables.DIRECTIVES or is_format_4(instruction)
 
+def is_format_4(instruction: str):
+    return instruction.startswith('+')
 
 preprocess_and_print_lines()
