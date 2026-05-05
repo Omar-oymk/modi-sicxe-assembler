ceb52681746d25d589ad8471982a98478020c060
abdul1ah33 <study.abdullah2005@gmail.com>
2026-05-05 09:24:26 +0300

    header part of htme record is now handles

---
+++ core/pass_2/htme/header.py
@@ -0,0 +1,28 @@
+
+
+
+def program_name(intermediate_table):
+    if intermediate_table[0].instruction.upper() == "START":
+        return intermediate_table[0].label if intermediate_table[0].label else "UNKNOWN"
+    return "UNKNOWN"
+
+def initial_counter(intermediate_table):
+    if intermediate_table[0].instruction.upper() == "START":
+        return intermediate_table[0].location_counter if intermediate_table[0].location_counter else 0
+    return 0
+
+def final_counter(block_table):
+    total_size = 0
+    for key in block_table:
+        total_size += block_table[key]["size"]
+    return total_size
+
+def total_length(intermediate_table, block_table):
+    return final_counter(block_table) - initial_counter(intermediate_table)
+
+
+def header_record(intermediate_table, block_table):
+    program_name_str = program_name(intermediate_table)
+    initial_counter_value = initial_counter(intermediate_table)
+    program_length = total_length(intermediate_table, block_table)
+    return f"H^{program_name_str}^{initial_counter_value:06X}^{program_length:06X}"
